dc002feab263193f03e7248e2b011a4bc98ad3ae
yigangxi <964496925@qq.com>
2018-09-07 14:58:35 +0800

    add packet

--- server.c
+++ server.c
@@ -8,22 +8,75 @@
 #include <string.h>
 #include <stdlib.h>
 
-void do_server(int client_sockfd){
-	char recvbuf[BUFSIZ];
-	while(1){
-    	memset(recvbuf,0,sizeof(recvbuf));
-        int ret = read(client_sockfd,recvbuf,sizeof(recvbuf));
-		if(ret == 0){
-			printf("client close\n");
+struct packet {
+	int len;
+	char buf[BUFSIZ];
+};
+
+ssize_t readn(int fd,void *buf,size_t count){
+	size_t nleft = count;
+	ssize_t nread;
+	char *bufp = (char *)buf;
+	while(nleft > 0){
+		if((nread = read(fd,bufp,nleft)) < 0){
+			if(errno == EINTR)
+				continue;
+			return 1;
+		}
+		else if(nread == 0)
 			break;
+		bufp += nread;
+		nleft -= nread;
+	}
+	return count;
+}
+
+ssize_t writen(int fd,const void *buf,size_t count){
+	size_t nleft = count;
+	ssize_t nwrite;
+	char *bufp = (char *)buf;
+	while(nleft > 0){
+		if((nwrite = write(fd,bufp,nleft)) < 0){
+			if(errno == EINTR)
+				continue;
+			return 1;
 		}
-		else if(ret ==-1){
+		else if(nwrite == 0)
+			continue;
+		bufp += nwrite;
+		nleft -= nwrite;
+	}
+	return count;
+}
+
+void do_server(int client_sockfd){
+	struct packet recvbuf;
+	int n;
+	while(1){
+		memset(&recvbuf,0,sizeof(recvbuf));
+		int ret = readn(client_sockfd,&recvbuf.len,4);
+		if(ret ==  -1){
 			perror("read error");
-			return -1;
+			return;
 		}
-        fputs(recvbuf,stdout);
-		write(client_sockfd,recvbuf,ret);
-	 }
+		else if(ret < 4){
+		    printf("client close\n");
+			break;
+	    }
+		n = ntohl(recvbuf.len);
+		ret = readn(client_sockfd,recvbuf.buf,n);
+		if(ret ==  -1){
+		     perror("read error");
+		     return;
+		}   
+		else if(ret < n){
+		     printf("client close\n");
+		     break;
+		}
+		n = ntohl(recvbuf.len);
+		fputs(recvbuf.buf,stdout);
+		writen(client_sockfd,&recvbuf,4+n);
+	}
 }
 
 int main(int argc, char *argv[]){
